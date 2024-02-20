92d3e6705c692485973536a55834f92f52465253
WORLD <109876481+NEEDGITGOOD@users.noreply.github.com>
2024-02-20 16:48:44 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,23 @@
+### Script Version 1.
+
+````bash
+curl -sSL https://raw.githubusercontent.com/NEEDGITGOOD/Linux_Schuluebung/main.sh | bash && cat info.txt
+````
+
+### Script Version 2.
+
+#### Install ncmli (network-manager)
+
+````bash
+apt-get install network-manager
+````
+
+#### Run Command
+
+````bash
+nmcli >info.txt && free >>info.txt && lsblk >>info.txt
+````
+
+### Disclaimer
+
+Use at your own Risk. Not responsible for anything.
